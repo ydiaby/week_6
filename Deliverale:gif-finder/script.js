256794fe7627e6d689cc0e59b91c3b1a18bcdd60
Christopher Berrios <chris.a.berrios@gmail.com>
2019-02-10 21:50:35 +0000

    Fixed cloning path

---
+++ Deliverale:gif-finder/script.js
@@ -0,0 +1,31 @@
+// Code out an event listener for the button that logs "Button pressed!" to the console when it is clicked
+
+
+// Go to https://developers.giphy.com/ and create an account. Then create your first app called testApp to create an API key. Enter it below.
+function sendApiRequest() {
+    fetch("https://api.giphy.com/v1/gifs/search?api_key=YOUR_API_KEY_GOES_HERE&q=Ryan+Gosling&limit=25&offset=0&rating=G&lang=en")
+    .then(function(data) {
+      return data.json();
+    })
+    .then(function(json) {
+      console.log(json);
+      // Pass the JSON on to the next function.
+
+    });
+};
+
+// Get a specific image URL ending in .gif from your JSON search results. Pass it on to the next function.
+function getImageURLfrom(myJSON) {
+
+};
+
+// querySelect the wrapper, and add an image tag to it. Interpolate the URL string from the previous function.
+function addImageToScreen(myURL) {
+
+};
+
+// Playtime challenges:
+// Find something / someone else (rewrite the endpoint so that we aren't searching for Ryan Gosling)
+// Instead of getting the first image, get a random image from the JSON results
+// REPLACE the contents of the wrapper instead of adding onto them
+// Make all your images the same height so that they line up nicely
