c959170643cb397987fceea2b88e6673cc4c7e39
Christopher Berrios <chris.a.berrios@gmail.com>
2019-02-11 12:41:24 +0000

    Updated Day one folder

---
+++ week_6_day_one_practice/README.md
@@ -0,0 +1,12 @@
+## Welcome to Week 6 Day One Practice Folder
+## This is all about JSON
+## JSON is an ancronym that stands for Javascript Object Notation and is the statndard for data formatting 
+## when it comes to RESTful API's (REpresentational State Transfer Application Program Interface)
+## That's a lot to deconstruct but all it is, is a method for packing data (in JSON) from one server or servers to a website or application
+## You've seen API's all over the place and never knew. Here's a brief list:
+## 1 - Relit Sign Up (https://repl.it/signup): On this page you'll see three icons (Google, Github, and Facebook). 
+## If you were logged in to either one of those accounts and then clicked on one of those icons. You'd be able to sign up for Replit without typing in any information. 
+## 2 - Google Maps : there are multiple sites that have embedded Google maps on them to show you the location of either their restaurant, store, etc.
+## 3 - Kayak : Going on a trip? Well if you are looking at flights request to look at prices. You'll be given a list of airline details from multiple different airlines including seat avaialble, prices and departure times.
+## Anyway let's get to practicing. 
+## Now open up your example.json file to begin your coding. 
---
+++ week_6_day_one_practice/app.js
@@ -0,0 +1,48 @@
+//1 - Paste your example.json code below and set it to a variable called information
+//JSON is an object data format, which is like an associative array working in key:value syntax. 
+// To get a the value of a key we use the following format variable.key
+ 
+
+console.log(information.id) // will print out 1
+
+//2 - console.log each item in the space below.
+
+
+
+
+//3 - Let's create two variables called button and wrapper and query select the wrapper and button from index.html
+
+
+
+//4 - Use the .innerHTML method on the wrapper variable to generate the following sentence:
+//Expected Output: "Hello"
+
+
+
+//5 Next concactenate the above message to your first name by calling the object value on it.
+//Expected Output: - Should read "Hello my name is Christopher"
+
+
+
+//6 Next create a click event listener for your button and console.log a message to make sure it works
+
+//7 Now inside your event listener add the above .innerHTML message so that it only happens when the "Click Me" button is clicked
+
+//8 Now concactenate all the object values inside of your click even listener.
+//Expected output: "Hello my name is Christopher Berrios, I am 29 years old and I am a high school teacher"
+
+
+Challenge Activities
+//9 Create your a JSON object of yourself. 
+//Must include your firstname, lastname, age, birthday, occupation
+
+//10 Make it so that the click button outputs all of your information instead of mine. 
+
+
+//11 Find an image of your spirit animal on Unsplash.com. Update your JSON object to include that image and have it appear when you click on your event listener. 
+
+
+
+
+
+
